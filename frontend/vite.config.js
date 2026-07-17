7fcf2b805524681637142ab56fb05337aaff7a2c
GutierrezCardenasAlex <gutierrezcardenasalex@gmail.com>
2026-07-16 23:44:52 -0400

    version 0.0.8

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,6 +5,10 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 5173,
-    allowedHosts: ["rutas.cybernovatech.space", "llegar_destino.cybernovatech.space"],
+    allowedHosts: [
+      "rutas.cybernovatech.space",
+      "llegar-destino.cybernovatech.space",
+      "llegar_destino.cybernovatech.space",
+    ],
   },
 });
